f7fa8cd3073c3a0a6f489c4d755a41d334cc7e05
BlazPocrnja <bpocrnja@gmail.com>
2017-04-06 23:18:05 -0600

    Finished Question 1: fourSquares()

--- 1472712.pl
+++ 1472712.pl
@@ -1,1 +1,17 @@
+/*
+CMPUT 325 Assignment 4
+Author: Blaz Pocrnja
+Student ID: 1472712
+*/
+
+:- use_module(library(clpfd)).
+
+/*	Question 1
+*/
+fourSquares(N, [S1, S2, S3, S4]) :- 
+	Vars = [S1, S2, S3, S4],
+	Vars ins 0..sup,
+	N #= S1*S1 + S2*S2 + S3*S3 + S4*S4,
+	S1 #=< S2, S2 #=< S3, S3 #=< S4,
+	label([S1, S2, S3, S4]).
 
